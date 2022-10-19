f944853184597f7941987ca992b5be47f5757fec
gidf43 <115552684+gidf43@users.noreply.github.com>
2022-10-19 23:11:14 +0100

    Add files via upload
    migrations

---
+++ webgenius/database/migrations/2022_10_19_163843_create_funcionario_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionario', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',30);
+            $table->date('data_nasc');
+            $table->string('genero');
+            $table->string('tipo_fun',10);
+            $table->string('telefone',20);
+            $table->string('email_fun');
+            $table->string('folha_salario');
+            $table->decimal('faltas');
+            $table->string('imagem_fun'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionario');
+    }
+};
---
+++ webgenius/database/migrations/2022_10_19_164632_create_aluno_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aluno', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_aluno');
+            $table->date('data_nasc');
+            $table->string('email_aluno');
+            $table->string('telefone_aluno');
+            $table->string('genero');
+            $table->double('nota_aluno');
+            $table->float('num_faltas',3);
+            $table->boolean('estado_aprovado');
+            $table->double('propina');
+            $table->string('imagem_aluno');
+            $table->string('senha_aluno');
+         
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aluno');
+    }
+};
